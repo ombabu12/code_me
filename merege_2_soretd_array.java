e3e277b749178671aff5a8704d2db7b8a082e059
Om Babu <140878465+ombabu12@users.noreply.github.com>
2025-01-25 14:30:34 +0530

    Add files via upload

---
+++ merege_2_soretd_array.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class merege_2_soretd_array {
+
+public static void merge(int[] nums1, int m, int[] nums2, int n){
+    int k=m+n-1;
+    int i=m-1;
+    int  j=n-1;
+    while(i>=0 && j>=0){
+        if(nums2[j]>nums1[i]){
+            nums1[k]=nums2[j];
+            j--;
+            k--;
+        }
+        else{
+            nums1[k]=nums1[i];
+            i--;
+            k--;
+        }
+    }
+    System.err.println(Arrays.toString(nums1));
+}
+    public static void main(String[] args) {
+    int nums1[]={1,2,3,0,0,0};
+    int m=3,n=3;
+    int nums2[]={2,5,6};
+    merge(nums1, m, nums2, n);
+}
+}
